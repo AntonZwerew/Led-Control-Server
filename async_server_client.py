993dfe76d7d00e21cb1595c31fbb7feb356793f4
Anton <anton.zwerew@gmail.com>
2019-11-13 02:53:34 +0300

    Начат графический интерфейс клиента

--- async_server_client.py
+++ async_server_client.py
@@ -2,12 +2,52 @@
 
 import socket
 from time import sleep
+from tkinter import *  # Чтобы не тащить Qt
 
 encoding = "utf-8"  # важно, иначе не пройдет сравнение на сервере
 address_to_server = ('localhost', 7777)
 
 client = None
 connected = False
+
+
+def create_interface():
+    root = Tk()
+    command_buttons_frame = Frame(root)
+    log_frame = Frame(root)
+
+    log = Label(log_frame)
+    log.pack()
+    log_frame.pack()
+
+    command_buttons = []
+    for command in get_available_commands():
+        command_buttons.append(Button(command_buttons_frame, text=command))
+    for button in command_buttons:
+        button.bind("<Button-1>", run_button_command)
+        button.pack()
+
+    command_buttons_frame.pack()
+    root.mainloop()
+
+
+def run_button_command(event):
+    global log
+    result = run_request(event.widget['text'])  # widget - на чем произошел ивент
+    log["text"] += result
+    return result
+
+
+def get_available_commands():
+    result = run_request("show-commands")
+    result = result.decode(encoding)
+    if result.startswith("OK ") and result.endswith("\n"):
+        result = result[3:]  # убираем "OK "
+        result = result[:-1]  # убираем "\n"
+    else:
+        # ответ некорректный
+        return
+    return result.split(", ")
 
 
 def ensure_connection():
@@ -28,20 +68,23 @@ def ensure_connection():
             sleep(2)
 
 
+def run_request(request):
+    try:
+        client.send(bytes(request + "\n", encoding=encoding))
+    except ConnectionRefusedError or ConnectionResetError:
+        ensure_connection()
+        if request != "":
+            print(f"request: {request}")
+            return run_request(request)
+    ans = client.recv(1024)  # TODO Если ответ больше
+    return ans
+
+
 ensure_connection()
+# create_interface()
 
 # TODO если успел сервер и были посланы данные - пересылать из после поднятиея сервера
 while True:
     request = input("request:")
-    try:
-        client.send(bytes(request + "\n", encoding=encoding))
-        ans = client.recv(1024)
-        print(f"response: {ans}")
-    except ConnectionResetError or ConnectionRefusedError:
-        ensure_connection()
-        if request != "":
-            client.send(bytes(request + "\n", encoding=encoding))
-            ans = client.recv(1024)
-            print(f"request: {request}")
-            print(f"response: {ans}")
+    print(f"response: {run_request(request)}")
 
